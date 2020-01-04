d7ac6c921999ef74299927627c5f50a8cd4162ed
frank-qcd-qk <cxq41@case.edu>
2020-01-04 00:32:05 -0500

    fix unit

--- SnowJokeFirmware/populateSpeed.ino
+++ SnowJokeFirmware/populateSpeed.ino
@@ -17,8 +17,8 @@ void actualObsSpeed(){
     // Get time
     long currentTime =  millis();
     
-    obsLeftMotorSpeed = ((currentLeftPos-lastLeftPos)*1000.0/1024.0)/(currentTime-lastEncoderTime)*60;
-    obsRightMotorSpeed = ((currentRightPos-lastRightPos)*1000.0/1024.0)/(currentTime-lastEncoderTime)*60;
+    obsLeftMotorSpeed = ((currentLeftPos-lastLeftPos)*1000.0/1024.0)/(currentTime-lastEncoderTime);
+    obsRightMotorSpeed = ((currentRightPos-lastRightPos)*1000.0/1024.0)/(currentTime-lastEncoderTime);
 
     lastLeftPos = currentLeftPos;
     lastRightPos = currentRightPos;
